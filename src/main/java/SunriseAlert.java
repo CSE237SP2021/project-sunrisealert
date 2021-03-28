a072f2be0b96baddd2c86fbd742e5fcefc1d6720
Elijah Pena <elijah.pena@wustl.edu>
2021-03-28 17:44:16 -0500

    Users can supply their own zipcode / city now

--- src/main/java/SunriseAlert.java
+++ src/main/java/SunriseAlert.java
@@ -7,42 +7,16 @@ public class SunriseAlert {
 
     public static void main(String args[]) throws Exception {
 
-//        String API_KEY = "2a7f891e21c64fe59f2114971a77e92d";
-//        System.out.println("Hello there");
-//        String url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=" + API_KEY;
-//        URL urlObj = new URL(url);
-//        HttpURLConnection connection = (HttpURLConnection) urlObj.openConnection();
-//        connection.setRequestMethod("GET");
-//        int responseCode = connection.getResponseCode();
-//        if (responseCode == HttpURLConnection.HTTP_OK) {
-//            BufferedReader inputReader = new BufferedReader(
-//                    new InputStreamReader(connection.getInputStream()));
-//            String inputLine;
-//            StringBuffer response = new StringBuffer();
-//
-//            while ((inputLine = inputReader.readLine()) != null) {
-//                response.append(inputLine);
-//            }
-//
-//            inputReader.close();
-//
-//            String json = response.toString();
-//            //Begin parsing json
-//            json = json.substring(json.indexOf("articles"));
-//
-//            String[] headlines = new String[20];
-//            for (int i = 0; i < 20; i++) {
-//                json = json.substring(json.indexOf("source") + 1);
-//                headlines[i] = json.substring(json.indexOf("title") + 7, json.indexOf("description") - 2);
-//                System.out.println(headlines[i]);
-//            }
-//
-//        }
         String[] headlines = getTopHeadlines();
         for (int i = 0; i < headlines.length; i++) {
             System.out.println(headlines[i]);
         }
-        System.out.println(getWeather("63130"));
+        if (args[0] == null) {
+
+        } else {
+            //args[0] holds the location passed by the user
+            System.out.println(getWeather(args[0]));
+        }
 
     }
 
@@ -82,7 +56,7 @@ public class SunriseAlert {
     }
 
     public static String getWeather(String location) throws Exception {
-        String url = "https://wttr.in/" + location + "?format=3";
+        String url = "https://wttr.in/" + location + "?format=\"%l:+%C+%t\"";
         URL urlObj = new URL(url);
         HttpURLConnection connection = (HttpURLConnection) urlObj.openConnection();
         connection.setRequestMethod("GET");
