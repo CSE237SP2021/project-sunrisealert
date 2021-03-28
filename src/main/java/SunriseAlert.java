7df2a94db9717174fd5885d0e90cc0d7ccd0d93f
Elijah Pena <elijah.pena@wustl.edu>
2021-03-28 17:11:11 -0500

    Methods are better

--- src/main/java/SunriseAlert.java
+++ src/main/java/SunriseAlert.java
@@ -7,8 +7,48 @@ public class SunriseAlert {
 
     public static void main(String args[]) throws Exception {
 
+//        String API_KEY = "2a7f891e21c64fe59f2114971a77e92d";
+//        System.out.println("Hello there");
+//        String url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=" + API_KEY;
+//        URL urlObj = new URL(url);
+//        HttpURLConnection connection = (HttpURLConnection) urlObj.openConnection();
+//        connection.setRequestMethod("GET");
+//        int responseCode = connection.getResponseCode();
+//        if (responseCode == HttpURLConnection.HTTP_OK) {
+//            BufferedReader inputReader = new BufferedReader(
+//                    new InputStreamReader(connection.getInputStream()));
+//            String inputLine;
+//            StringBuffer response = new StringBuffer();
+//
+//            while ((inputLine = inputReader.readLine()) != null) {
+//                response.append(inputLine);
+//            }
+//
+//            inputReader.close();
+//
+//            String json = response.toString();
+//            //Begin parsing json
+//            json = json.substring(json.indexOf("articles"));
+//
+//            String[] headlines = new String[20];
+//            for (int i = 0; i < 20; i++) {
+//                json = json.substring(json.indexOf("source") + 1);
+//                headlines[i] = json.substring(json.indexOf("title") + 7, json.indexOf("description") - 2);
+//                System.out.println(headlines[i]);
+//            }
+//
+//        }
+        String[] headlines = getTopHeadlines();
+        for (int i = 0; i < headlines.length; i++) {
+            System.out.println(headlines[i]);
+        }
+        System.out.println(getWeather("63130"));
+
+    }
+
+    public static String[] getTopHeadlines() throws Exception {
+        String[] error = new String[1];
         String API_KEY = "2a7f891e21c64fe59f2114971a77e92d";
-        System.out.println("Hello there");
         String url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=" + API_KEY;
         URL urlObj = new URL(url);
         HttpURLConnection connection = (HttpURLConnection) urlObj.openConnection();
@@ -34,12 +74,11 @@ public class SunriseAlert {
             for (int i = 0; i < 20; i++) {
                 json = json.substring(json.indexOf("source") + 1);
                 headlines[i] = json.substring(json.indexOf("title") + 7, json.indexOf("description") - 2);
-                System.out.println(headlines[i]);
             }
-
+            return headlines;
         }
-        System.out.println(getWeather("63130"));
 
+        return error;
     }
 
     public static String getWeather(String location) throws Exception {
