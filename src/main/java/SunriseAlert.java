9dc1cc8491b002fccf67b8841ebdb6426b8e4c72
Daniel Ruffing <danielruffing@wustl.edu>
2021-03-28 20:04:43 -0500

    refactored the two api calls

--- src/main/java/SunriseAlert.java
+++ src/main/java/SunriseAlert.java
@@ -2,12 +2,14 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import api.NewsApiClient;
+import api.WeatherApiClient;
 
 public class SunriseAlert {
-
     public static void main(String args[]) throws Exception {
-
-        String[] headlines = getTopHeadlines();
+        NewsApiClient news = new NewsApiClient();
+        WeatherApiClient weather = new WeatherApiClient();
+        String[] headlines = news.getTopHeadlines();
         for (int i = 0; i < headlines.length; i++) {
             System.out.println(headlines[i]);
         }
@@ -15,66 +17,8 @@ public class SunriseAlert {
 
         } else {
             //args[0] holds the location passed by the user
-            System.out.println(getWeather(args[0]));
+            System.out.println(weather.getWeather("63130"));
         }
 
-    }
-
-    public static String[] getTopHeadlines() throws Exception {
-        String[] error = new String[1];
-        String API_KEY = "2a7f891e21c64fe59f2114971a77e92d";
-        String url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=" + API_KEY;
-        URL urlObj = new URL(url);
-        HttpURLConnection connection = (HttpURLConnection) urlObj.openConnection();
-        connection.setRequestMethod("GET");
-        int responseCode = connection.getResponseCode();
-        if (responseCode == HttpURLConnection.HTTP_OK) {
-            BufferedReader inputReader = new BufferedReader(
-                    new InputStreamReader(connection.getInputStream()));
-            String inputLine;
-            StringBuffer response = new StringBuffer();
-
-            while ((inputLine = inputReader.readLine()) != null) {
-                response.append(inputLine);
-            }
-
-            inputReader.close();
-
-            String json = response.toString();
-            //Begin parsing json
-            json = json.substring(json.indexOf("articles"));
-
-            String[] headlines = new String[20];
-            for (int i = 0; i < 20; i++) {
-                json = json.substring(json.indexOf("source") + 1);
-                headlines[i] = json.substring(json.indexOf("title") + 7, json.indexOf("description") - 2);
-            }
-            return headlines;
-        }
-
-        return error;
-    }
-
-    public static String getWeather(String location) throws Exception {
-        String url = "https://wttr.in/" + location + "?format=\"%l:+%C+%t\"";
-        URL urlObj = new URL(url);
-        HttpURLConnection connection = (HttpURLConnection) urlObj.openConnection();
-        connection.setRequestMethod("GET");
-        int responseCode = connection.getResponseCode();
-        if (responseCode == HttpURLConnection.HTTP_OK) {
-            BufferedReader inputReader = new BufferedReader(
-                    new InputStreamReader(connection.getInputStream()));
-            String inputLine;
-            StringBuffer response = new StringBuffer();
-
-            while ((inputLine = inputReader.readLine()) != null) {
-                response.append(inputLine);
-            }
-
-            inputReader.close();
-            return response.toString();
-
-        }
-        return "Failed";
     }
 }
