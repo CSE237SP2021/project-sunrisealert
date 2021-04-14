727a9b0120e22d54bc493d519577f559dca74981
Daniel Ruffing <danielruffing@wustl.edu>
2021-04-14 11:17:41 -0500

    added location functionality

--- SunriseAlerts.sh
+++ SunriseAlerts.sh
@@ -1,4 +1,19 @@
-echo "Good morning $1"
+echo "Welcome to Sunrise Alerts!"
+
+echo "Please type your name [leave blank if undesired]"
+read name
+
+echo "Please type a city or area code for weather [IP by default]"
+read location
+
 cd src/main/java
 javac SunriseAlert.java
-java SunriseAlert
+
+if [[ -n "$name" ]]; then
+	echo "Good morning $name"
+fi
+
+if [[ -n "$location" ]]; then
+	java SunriseAlert $location
+else java SunriseAlert
+fi
